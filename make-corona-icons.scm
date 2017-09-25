c46d586b235fbc7c5b72f8925ca5e8b4869ed5db
Greg McDonald <gregmcdo@gmail.com>
2017-09-25 12:29:48 -0400

    update for iOS 11 and Corona SDK requirements

--- make-corona-icons.scm
+++ make-corona-icons.scm
@@ -9,8 +9,8 @@
 			(width (car (gimp-image-width img)) )
 			(height (car (gimp-image-height img)) )
 
-			(targetW 180)
-			(targetH 180)
+			(targetW 1024)
+			(targetH 1024)
 	
 			(halfW (* 0.5 width))
 			(halfH (* 0.5 height))
@@ -22,10 +22,8 @@
 		)
 
 
-
-		(set! filename "Icon-60@3x.png")
-		(set! targetW 180)
-		(set! targetH 180)
+		(set! filename "Icon-1024.png")
+	
 		(set! fullFilename folder-path)
 		(set! fullFilename (string-append fullFilename filename))
 		(set! filename fullFilename)
@@ -43,6 +41,31 @@
 							0
 							0
 		)
+
+
+
+
+		(set! filename "Icon-180.png")
+		(set! targetW 180)
+		(set! targetH 180)
+		(set! fullFilename folder-path)
+		(set! fullFilename (string-append fullFilename filename))
+		(set! filename fullFilename)
+		(gimp-image-scale img targetW targetH)
+		(file-png-save 	RUN-NONINTERACTIVE
+							img
+							drawable
+							filename
+							filename
+							0
+							9
+							1
+							0
+							0	
+							0
+							0
+		)
+
 
 
 		(set! filename "Icon-xxxhdpi.png")
@@ -91,7 +114,7 @@
 
 
 		
-		(set! filename "Icon-76@2x.png")
+		(set! filename "Icon-152.png")
 		(set! targetW 152)
 		(set! targetH 152)
 		(set! fullFilename folder-path)
@@ -112,31 +135,14 @@
 							0
 		)
 
-		
-		(set! filename "Icon-72@2x.png")
+	
+	
+		(set! filename "Icon-xxhdpi.png")
 		(set! targetW 144)
 		(set! targetH 144)
 		(set! fullFilename folder-path)
 		(set! fullFilename (string-append fullFilename filename))
 		(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
-		(set! filename "Icon-xxhdpi.png")
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-		(set! filename fullFilename)
 		(file-png-save 	RUN-NONINTERACTIVE
 							img
 							drawable
@@ -152,7 +158,7 @@
 		)
 
 
-		(set! filename "Icon-Small-40@3x.png")
+		(set! filename "Icon-120.png")
 		(set! targetW 120)
 		(set! targetH 120)
 		(set! fullFilename folder-path)
@@ -172,93 +178,12 @@
 							0
 							0
 		)
-		
-
-		(set! filename "Icon-60@2x.png")
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
 
 
-		(set! filename "Icon@2x.png")
-		(set! targetW 114)
-		(set! targetH 114)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
-
-		(set! filename "Icon-Small-50@2x.png")
-		(set! targetW 100)
-		(set! targetH 100)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
-
-		(set! filename "Icon-Small-50@2x.png")
-		(set! targetW 100)
-		(set! targetH 100)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
 
 		
 
-			(set! filename "Icon-xhdpi.png")
+		(set! filename "Icon-xhdpi.png")
 		(set! targetW 96)
 		(set! targetH 96)
 		(set! fullFilename folder-path)
@@ -278,6 +203,29 @@
 							0
 							0
 		)
+
+		(set! filename "Icon-87.png")
+		(set! targetW 87)
+		(set! targetH 87)
+		(set! fullFilename folder-path)
+		(set! fullFilename (string-append fullFilename filename))
+		(set! filename fullFilename)
+		(gimp-image-scale img targetW targetH)
+		(file-png-save 	RUN-NONINTERACTIVE
+							img
+							drawable
+							filename
+							filename
+							0
+							9
+							1
+							0
+							0
+							0
+							0
+		)
+
+
 
 			(set! filename "Icon-hdpi.png")
 		(set! targetW 72)
@@ -300,30 +248,7 @@
 							0
 		)
 
-
-
-		(set! filename "Icon-Small@3x.png")
-		(set! targetW 87)
-		(set! targetH 87)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
-
-		(set! filename "Icon-Small-40@2x.png")
+		(set! filename "Icon-80.png")
 		(set! targetW 80)
 		(set! targetH 80)
 		(set! fullFilename folder-path)
@@ -365,50 +290,9 @@
 							0
 		)
 
-		(set! filename "Icon-72.png")
-		(set! targetW 72)
-		(set! targetH 72)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
-
-		(set! filename "Icon-60.png")
-		(set! targetW 60)
-		(set! targetH 60)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
 
 		
-		(set! filename "Icon-Small@2x.png")
+		(set! filename "Icon-58.png")
 		(set! targetW 58)
 		(set! targetH 58)
 		(set! fullFilename folder-path)
@@ -450,28 +334,8 @@
 							0
 		)
 
-		(set! filename "Icon-Small-50.png")
-		(set! targetW 50)
-		(set! targetH 50)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
 
-			(set! filename "Icon-mdpi.png")
+		(set! filename "Icon-mdpi.png")
 		(set! targetW 48)
 		(set! targetH 48)
 		(set! fullFilename folder-path)
@@ -492,7 +356,7 @@
 							0
 		)
 		
-		(set! filename "Icon-Small-40.png")
+		(set! filename "Icon-40.png")
 		(set! targetW 40)
 		(set! targetH 40)
 		(set! fullFilename folder-path)
@@ -513,7 +377,7 @@
 							0
 		)
 
-			(set! filename "Icon-ldpi.png")
+		(set! filename "Icon-ldpi.png")
 		(set! targetW 36)
 		(set! targetH 36)
 		(set! fullFilename folder-path)
@@ -534,26 +398,6 @@
 							0
 		)
 
-		(set! filename "Icon-Small.png")
-		(set! targetW 29)
-		(set! targetH 29)
-		(set! fullFilename folder-path)
-		(set! fullFilename (string-append fullFilename filename))
-				(set! filename fullFilename)
-		(gimp-image-scale img targetW targetH)
-		(file-png-save 	RUN-NONINTERACTIVE
-							img
-							drawable
-							filename
-							filename
-							0
-							9
-							1
-							0
-							0
-							0
-							0
-		)
 
 
 	)
@@ -569,7 +413,7 @@
 	Android app Corona SDK."		
 	"Greg McDonald"						
 	"GPL"						
-	"January 11, 2017"						
+	"September 25, 2017"						
 	""									
 	SF-IMAGE		"Image" 0					
 	SF-DRAWABLE	"Layer" 0				 )
@@ -577,8 +421,6 @@
 (script-fu-menu-register "make-corona-icons"
 			"<Image>/Script-Fu")
 	  
-
-
 
 
 
